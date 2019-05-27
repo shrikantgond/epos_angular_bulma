53e4c6c465b828928df9a37f59ce82c3c5eea009
shrikantgond@gmail.com <shrikantgond@gmail.com>
2019-05-27 19:30:22 +0530

    added documents

--- src/app/models/vendor.model.ts
+++ src/app/models/vendor.model.ts
@@ -6,17 +6,18 @@ export interface IVendorModel{
     mobile: number;
     email: string;
     credit:number;
+    maxcredit:number;
     address:string;
     //inventoryitems?: IInventoryModel[];
   }
 
   export function DefaultVendors(): IVendorModel[] {
     return [
-        {title:"Shrikant Pvt. Ltd.",mobile:9421883354,email:"shrikant@gond.com",credit:24,address:"B-603, Ramchandra Nagar Apt..."}
-      ,{title:"Avinash Traders Pvt. Ltd.",mobile:9421883354,email:"shrikant@gond.com",credit:24,address:"B-603, Ramchandra Nagar Apt..."}
-      ,{title:"Minutai Caterers",mobile:9421883354,email:"shrikant@gond.com",credit:24,address:"B-603, Ramchandra Nagar Apt..."}
-      ,{title:"Tukamram Pande mithai",mobile:9421883354,email:"shrikant@gond.com",credit:24,address:"B-603, Ramchandra Nagar Apt..."}
-      ,{title:"Mohani traders",mobile:9421883354,email:"shrikant@gond.com",credit:24,address:"B-603, Ramchandra Nagar Apt..."}
-      ,{title:"Akshay mills",mobile:9421883354,email:"shrikant@gond.com",credit:24,address:"B-603, Ramchandra Nagar Apt..."}
+        {title:"Shrikant Pvt. Ltd.",mobile:9421883354,email:"shrikant@gond.com",credit:24,maxcredit:100,address:"B-603, Ramchandra Nagar Apt..."}
+      ,{title:"Avinash Traders Pvt. Ltd.",mobile:9421883354,email:"shrikant@gond.com",credit:24,maxcredit:100,address:"B-603, Ramchandra Nagar Apt..."}
+      ,{title:"Minutai Caterers",mobile:9421883354,email:"shrikant@gond.com",credit:24,maxcredit:100,address:"B-603, Ramchandra Nagar Apt..."}
+      ,{title:"Tukamram Pande mithai",mobile:9421883354,email:"shrikant@gond.com",credit:24,maxcredit:100,address:"B-603, Ramchandra Nagar Apt..."}
+      ,{title:"Mohani traders",mobile:9421883354,email:"shrikant@gond.com",credit:24,maxcredit:100,address:"B-603, Ramchandra Nagar Apt..."}
+      ,{title:"Akshay mills",mobile:9421883354,email:"shrikant@gond.com",credit:24,maxcredit:100,address:"B-603, Ramchandra Nagar Apt..."}
     ]
 }
